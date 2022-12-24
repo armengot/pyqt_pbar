43e05fc11b25b55237b03cb6cf7c6765bf721594
Marcelo Armengot <marcelo.armengot@izertis.com>
2022-12-24 19:13:32 +0100

    Progress bar win example in PyQt to get data.

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # Progress bar example in PyQt5
 Many examples in the www of progress bar in PyQt as a single MainWindow.
-I needed to make a progress bar as an info progress single window inside a project (not here).
+I needed to make a progress bar as an info progress single window inside a project (not here) that returns data worked in progress.
 
-So here is the way.
+So here is the example.
 
-# Next commit
-How to get info of the progressing class to the win and return to the barwin call.
+Call to get the output data:
+```
+self.data = barwin.get(None,"Progress load",5)   
+```
+
+Some similar can be done to send more structured data to the player class.
+
 
--- pyqt_bar.py
+++ pyqt_bar.py
@@ -6,22 +6,24 @@ import sys
 # class to make the progressing work
 class player(QObject):
     progress = signal(int)
-    completed = signal(int)
+    completed = signal(list)
 
     @slot(int)
     def do_work(self, n):
+        data =[] # work output
         for i in range(1, n+1):
             time.sleep(1)
             print("working")
             # LOOP AREA
             # ...
             # ...
+            data.append(1000+i)
             # ...
             # ...
             # END LOOP AREA
             self.progress.emit(i)
 
-        self.completed.emit(i)
+        self.completed.emit(data)
 
 class barwin(QProgressDialog):
     work_requested = signal(int)  
@@ -34,6 +36,8 @@ class barwin(QProgressDialog):
         self.worker = player()
         self.worker_thread = QThread()
         self.setCancelButton(None)
+        # data in progress
+        self.data = []
 
         # signal connections
         self.worker.progress.connect(self.update_progress)
@@ -50,18 +54,24 @@ class barwin(QProgressDialog):
         #self.show()
         self.work_requested.emit(limit)
 
-
     def update_progress(self, v):
         print("signal arrived update")
         print(v)
         self.setValue(v)     
 
-    def complete(self, v):
+    def complete(self, back):
         print("signal arrived complete")
         print("stops")
-        self.setValue(v) 
+        self.setValue(len(back)) 
+        self.data = back
         self.worker_thread.quit()
         self.worker_thread.wait()
+
+    @staticmethod
+    def get(parent,title,n):
+        bwin = barwin(title,n)
+        bwin.exec_()
+        return(bwin.data) 
 
 # ----------------------------------------------------------------
 class main(QMainWindow):
@@ -73,11 +83,13 @@ class main(QMainWindow):
         self.btn_start.setMaximumWidth(200)
         self.btn_start.setMaximumHeight(100)              
         self.layout().addWidget(self.btn_start)
+        self.data = []
 
     def start(self):
         print("START")
-        bar = barwin("progress",5)
-        bar.exec()
+        self.data = barwin.get(None,"Progress load",5)        
+        print(self.data)
+
 # ----------------------------------------------------------------
 app = QApplication(sys.argv)
 window = main()
